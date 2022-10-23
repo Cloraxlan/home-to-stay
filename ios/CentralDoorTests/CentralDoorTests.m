3a0784e9870b7f369c712c8ff81b36b476223f58
Konrad Rozpadek <sbluek546@gmail.com>
2022-10-22 23:05:29 -0500

    renamed app to Central Door

--- ios/CentralDoorTests/CentralDoorTests.m
+++ ios/CentralDoorTests/CentralDoorTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnativetsTests : XCTestCase
+@interface CentralDoorTests : XCTestCase
 
 @end
 
-@implementation rnativetsTests
+@implementation CentralDoorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
