7ed0380b43665f11587315f782ee1791571bb0b6
Konrad Rozpadek <sbluek546@gmail.com>
2022-12-14 17:49:19 -0600

    tiles created

--- components/housing/Housing.tsx
+++ components/housing/Housing.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import HomeButton from "../homepage/HomeButton";
+import Tile from "../tiles/Tile";
+import HousingTile from "./HousingTile";
 
 const Housing = () => {
 	return (
 		<View>
 			<HomeButton />
-			<Text>Housing</Text>
+			<View style={styles.tiles}>
+				<Tile icon={"houseIcon"} title="title">
+					<Text>hihi</Text>
+				</Tile>
+			</View>
 		</View>
 	);
 };
 
 export default Housing;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	tiles: {
+		flexDirection: "row",
+		justifyContent: "center",
+		padding: "5%",
+	},
+});
