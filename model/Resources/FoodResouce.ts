c9483fe10590819b77e4f56c4fe53838949ade77
Konrad Rozpadek <sbluek546@gmail.com>
2023-10-22 16:21:17 -0500

    fix constructors

--- model/Resources/FoodResouce.ts
+++ model/Resources/FoodResouce.ts
@@ -2,6 +2,7 @@ import { Resource } from "./Resource";
 import { Phone } from "../Clickables/Phone";
 import { Address } from "../Clickables/Address";
 import { URL } from "../Clickables/URL";
+import { Email } from "../Clickables/Email";
 
 export class FoodResouce extends Resource {
 	constructor(
@@ -10,8 +11,9 @@ export class FoodResouce extends Resource {
 		address?: Address,
 		link?: URL,
 		phone?: Phone,
+		email?: Email,
 	) {
-		super(header, description, address, link, phone);
+		super(header, description, address, link, phone, email);
 		this._icon = require("./icons/foodIcon.png");
 	}
 }
--- model/Resources/HousingResource.tsx
+++ model/Resources/HousingResource.tsx
@@ -2,6 +2,7 @@ import { Resource } from "./Resource";
 import { Phone } from "../Clickables/Phone";
 import { Address } from "../Clickables/Address";
 import { URL } from "../Clickables/URL";
+import { Email } from "../Clickables/Email";
 
 export class HousingResouce extends Resource {
 	constructor(
@@ -10,8 +11,9 @@ export class HousingResouce extends Resource {
 		address?: Address,
 		link?: URL,
 		phone?: Phone,
+		email?: Email,
 	) {
-		super(header, description, address, link, phone);
+		super(header, description, address, link, phone, email);
 		this._icon = require("./icons/housing.png");
 	}
 }
