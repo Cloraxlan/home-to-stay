62b59bd545bde3c218b9e3f94e460d7e6edba3c1
Konrad Rozpadek <sbluek546@gmail.com>
2024-01-08 21:04:53 -0600

    better background

--- components/MilwaukeeBackground1.tsx
+++ components/MilwaukeeBackground1.tsx
@@ -1,9 +1,19 @@
 import { ImageBackground, StyleSheet, Text, View } from "react-native";
-import React from "react";
-
+import React, { useState } from "react";
+let bg1 = require("../backgrounds/bg1.jpg");
+let bg2 = require("../backgrounds/bg2.jpg");
+let bg3 = require("../backgrounds/bg3.jpg");
+let bg4 = require("../backgrounds/bg4.jpg");
+let bg5 = require("../backgrounds/bg5.jpg");
+let bg6 = require("../backgrounds/bg6.jpg");
 const MilwaukeeBackground1 = ({ children }: any) => {
+	let bgs = [bg1, bg2, bg3, bg4, bg5, bg6];
+	const max = bgs.length - 1;
+	console.log("BG!", bgs);
+	const [bg, setBg] = useState(bgs[Math.floor(Math.random() * max)]);
+
 	return (
-		<ImageBackground source={require("./bg5.jpg")} resizeMode="cover">
+		<ImageBackground source={bg} resizeMode="cover">
 			{children}
 		</ImageBackground>
 	);
